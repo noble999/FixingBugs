5d59ea4bea9b330a515da4fa1f714f40bac87dd6
noble999 <nleduc2@mail.csuchico.edu>
2019-09-16 16:18:39 -0700

    new 08

--- 08.cpp
+++ 08.cpp
@@ -5,12 +5,16 @@ using namespace std;
 
 int main()
 {
-  cout << "Please enter a number between 1 and 10: \n";
   int somenum;
+  cout << "Please enter a number between 1 and 10: \n";
   cin >> somenum;
   if(somenum >= 1 || somenum <= 10)
+  {
     cout << "Your number is within the requested range!\n";
+  }
   else
+  {
     cout << "No, you didn't follow directions. I asked for a number between 1 and 10.\n";
+  }
   return 0;
 }
