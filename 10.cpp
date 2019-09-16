ea6a3ffdf541e75748c124aa81fa5e7887e0fa93
noble999 <nleduc2@mail.csuchico.edu>
2019-09-16 16:21:56 -0700

    new 10

--- 10.cpp
+++ 10.cpp
@@ -9,8 +9,12 @@ int main()
   cout << "What is your favorite number?";
   cin >> favnum;
   if(favnum=6)
+  {
     cout << "Six is my favorite number too!" << endl;
+  }
   else
+  {
     cout << "That is not my favorite number." << endl;
+  }
   return 0;
 }
